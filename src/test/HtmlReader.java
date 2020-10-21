3eb0d93532d52d4fe5e1a85e9750c4d60402e695
Sadamune <zwiebelshi@gmail.com>
2020-10-21 23:49:21 +0900

    create HtmlReader

--- src/test/HtmlReader.java
+++ src/test/HtmlReader.java
@@ -2,19 +2,19 @@ package test;
 
 import java.io.File;
 import java.io.IOException;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
-public class ReadHtml {
+public class HtmlReader {
 
 	public static void main(String[] args) throws IOException {
 //		Document doc = Jsoup.connect("https://suumo.jp/ms/chuko/tokyo/sc_toshima/nc_94589774/").get();
 		Document doc = Jsoup.parse(new File("C:/Users/zwieb/Documents/test.html"),"utf-8");
         String title = doc.title();
         System.out.println("title is: " + title);
-        String keywords = doc.select("meta[name=keywords]").first().attr("content");
-        System.out.println("Meta keyword : " + keywords);
+        Element target = doc.select("script").first();
+        System.out.println(target);
 	}
 }
