478dcbdf7ec24249d55e4816399962cd9e44de30
Sadamune <zwiebelshi@gmail.com>
2020-11-02 23:53:53 +0900

    parse the html table as a json String by houseJsoupTest()

--- src/com/ssadamune/exercise/Test.java
+++ src/com/ssadamune/exercise/Test.java
@@ -16,9 +16,25 @@ public class Test {
         System.out.println(join);
     }
 
-    // 私道負担・道路, 諸費用, 建ぺい率・容積率, 入居時期
+    // 私道負担・道路, 諸費用, 建物面積, 土地面積, 建ぺい率・容積率, 入居時期
     // 土地の権利形態, 構造・工法, 用途地域, 地目
     // その他制限事項, その他概要・特記事項
+    // view-source:https://suumo.jp/chukoikkodate/tokyo/sc_toshima/nc_94436214/bukkengaiyo/
+    static void houseJsoupTest(String todofuken, int ucCode) throws IOException {
+        String hsUrl = "https://suumo.jp/chukoikkodate/tokyo/sc_" + todofuken + "/nc_" + ucCode + "/bukkengaiyo/";
+        Document doc = Jsoup.connect(hsUrl).get();
+        Elements thtdElements = doc.select("table[summary=表]").first().select("tr > *");
+        for (Element thtd : thtdElements) {
+            if (thtd.is("th")) {
+                System.out.println("th: " + thtd.children().first().text());
+            } else if (thtd.is("td")) {
+                System.out.println("td: " + thtd.text());
+            }
+        }
+
+    }
+
+    /*
     static void houseJsoupTest(String todofuken, int ucCode) throws IOException {
         String hsUrl = "https://suumo.jp/chukoikkodate/tokyo/sc_" + todofuken + "/nc_" + ucCode + "/bukkengaiyo/";
         Document doc = Jsoup.connect(hsUrl).get();
@@ -34,6 +50,7 @@ public class Test {
             }
         }
     }
+    */
 
     static String area(String tdText) {
         String areaPattern = "(\\D*)(\\d{2,}\\.\\d{2,})(\\D*)";
@@ -41,7 +58,7 @@ public class Test {
         return m.find() ? m.group(2) : "not found";
     }
 
-    // 管理費, 修繕積立金, 修繕積立基金, 諸費用, その他面積, 入居時期
+    // 管理費, 修繕積立金, 修繕積立基金, 諸費用, 専有面積, その他面積, 入居時期
     // 所在階, 構造・階建て, 敷地面積, 敷地の権利形態, 用途地域
     // その他概要・特記事項
     static void mansionJsoupTest(String todofuken, int ucCode) throws IOException {
@@ -56,11 +73,12 @@ public class Test {
         }
     }
 
+    {
     // 交通
-
+    }
 
     public static void main(String[] args) throws IOException {
         houseJsoupTest("toshima", 94436214);
-        mansionJsoupTest("toshima", 94332373);
+//        mansionJsoupTest("toshima", 94332373);
     }
 }
