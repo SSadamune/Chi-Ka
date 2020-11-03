8b33f11a2003339e24389d65b80f415fbc952ce3
Sadamune <zwiebelshi@gmail.com>
2020-11-03 23:26:41 +0900

    complete buildingCoverageFloorAreaRatio() and rightForm()

--- src/com/ssadamune/exercise/Test.java
+++ src/com/ssadamune/exercise/Test.java
@@ -16,49 +16,103 @@ public class Test {
         System.out.println(join);
     }
 
-    // 私道負担・道路, 諸費用, 建物面積, 土地面積, 建ぺい率・容積率, 入居時期
+    // 私道負担・道路, 諸費用, 建物面積, 土地面積, 建ぺい率・容積率
     // 土地の権利形態, 構造・工法, 用途地域, 地目
     // その他制限事項, その他概要・特記事項
     // view-source:https://suumo.jp/chukoikkodate/tokyo/sc_toshima/nc_94436214/bukkengaiyo/
-    static void houseJsoupTest(String todofuken, int ucCode) throws IOException {
+    static String houseJsoupTest(String todofuken, int ucCode) throws IOException {
         String hsUrl = "https://suumo.jp/chukoikkodate/tokyo/sc_" + todofuken + "/nc_" + ucCode + "/bukkengaiyo/";
         Document doc = Jsoup.connect(hsUrl).get();
         Elements thtdElements = doc.select("table[summary=表]").first().select("tr > *");
+        StringBuffer json = new StringBuffer("{\n" + "    \"ID\" : " + ucCode + "\n");
+        String curItem = "";
         for (Element thtd : thtdElements) {
             if (thtd.is("th")) {
-                System.out.println("th: " + thtd.children().first().text());
+                curItem = thtd.children().first().text();
             } else if (thtd.is("td")) {
-                System.out.println("td: " + thtd.text());
+                switch (curItem) {
+                case "私道負担・道路" :
+                    json.append("    \"" + curItem + "\" : \"" + thtd.text() + "\",\n");
+                    break;
+                case "諸費用" :
+                    json.append("    \"" + curItem + "\" : \"" + thtd.text() + "\",\n");
+                    break;
+                case "建物面積" :
+                case "土地面積" :
+                    json.append("    \"" + curItem + "\" : " + area(thtd.text()) + ",\n");
+                    break;
+                case "建ぺい率・容積率" :
+                    float[] bf = buildingCoverageFloorAreaRatio(thtd.text());
+                    json.append("    \"建ぺい率\" : " + bf[0] + ",\n");
+                    json.append("    \"容積率\" : " + bf[1] + ",\n");
+                    break;
+                case "土地の権利形態" :
+                    String[] rf = rightForm(thtd.text());
+                    json.append("    \"土地の権利形態\" : \"" + rf[0] + "\",\n");
+                    json.append("    \"借地期間\" : \"" + rf[1] + "\",\n");
+                    break;
+                }
             }
         }
-
+        json.append("}");
+        System.out.println(json);
+        return json.toString();
     }
 
-    /*
-    static void houseJsoupTest(String todofuken, int ucCode) throws IOException {
-        String hsUrl = "https://suumo.jp/chukoikkodate/tokyo/sc_" + todofuken + "/nc_" + ucCode + "/bukkengaiyo/";
-        Document doc = Jsoup.connect(hsUrl).get();
-
-        Elements trElements = doc.select("tr");
-
-        for (Element tr : trElements) {
-            Elements divText = tr.select("div");
-            if (divText.text().contains("建物面積")) {
-                System.out.println("建物面積：" + area(tr.select("td").text()));
-            } else if (divText.text().contains("土地面積")) {
-                System.out.println("土地面積：" + area(tr.select("td").text()));
-            }
-        }
-    }
-    */
-
+    // building area & land area
     static String area(String tdText) {
-        String areaPattern = "(\\D*)(\\d{2,}\\.\\d{2,})(\\D*)";
-        Matcher m = Pattern.compile(areaPattern).matcher(tdText);
+        // 112.86m2
+        Matcher m = Pattern.compile("(\\D*)(\\d{2,}\\.\\d{2,})(\\D*)").matcher(tdText);
         return m.find() ? m.group(2) : "not found";
     }
 
-    // 管理費, 修繕積立金, 修繕積立基金, 諸費用, 専有面積, その他面積, 入居時期
+    // building coverage ratio / floor area ratio
+    static float[] buildingCoverageFloorAreaRatio(String tdText) {
+        // "60％・160％"
+        Matcher m = Pattern.compile("(\\D*)(\\d+)(％\\D*)(\\d+)(％\\D*)").matcher(tdText);
+        float[] bcfa = new float[2];
+        if (m.find()) {
+            bcfa[0] = (float)Integer.parseInt(m.group(2))/100;
+            bcfa[1] = (float)Integer.parseInt(m.group(4))/100;
+        }
+        return bcfa;
+    }
+
+    // land rights form
+    static String[] rightForm(String tdText) {
+        // "賃借権（旧）、借地期間残存14年8ヶ月"
+        boolean isLeaseRight = false;
+        String rightForm;
+        String landLeasePeriod = new String();
+        if (tdText.contains("所有権")) {
+            rightForm = "所有権";
+        } else if (tdText.contains("賃借権（旧）")) {
+            rightForm = "賃借権（旧）";
+            isLeaseRight = true;
+        } else if (tdText.contains("賃借権（新）")) {
+            rightForm = "賃借権（新）";
+            isLeaseRight = true;
+        } else {
+            rightForm = "そのほか";
+        }
+        if (isLeaseRight) {
+            Matcher m = Pattern.compile("(\\D*)((\\d+)(年))?((\\d+)(ヶ月))?").matcher(tdText);
+            if (m.find()) {
+                String year = (m.group(3) == null) ? "0" : m.group(3);
+                String month = (m.group(6) == null) ? "0" : m.group(6);
+                landLeasePeriod = year + "-" + month;
+            }
+        }
+        return new String[]{rightForm, landLeasePeriod};
+    }
+
+    // 諸費用
+    static int expenses(String tdText) {
+        int expenses = 0;
+        return expenses;
+    }
+
+    // 管理費, 修繕積立金, 修繕積立基金, 諸費用, 専有面積, その他面積
     // 所在階, 構造・階建て, 敷地面積, 敷地の権利形態, 用途地域
     // その他概要・特記事項
     static void mansionJsoupTest(String todofuken, int ucCode) throws IOException {
@@ -78,7 +132,10 @@ public class Test {
     }
 
     public static void main(String[] args) throws IOException {
+        houseJsoupTest("toshima", 93427000);
+        houseJsoupTest("toshima", 95013699);
         houseJsoupTest("toshima", 94436214);
+        houseJsoupTest("setagaya", 94826730);
 //        mansionJsoupTest("toshima", 94332373);
     }
 }
