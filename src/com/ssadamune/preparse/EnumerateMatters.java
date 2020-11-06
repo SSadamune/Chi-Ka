b317b789143ea919b16e15701efb8670e98ba188
Sadamune <zwiebelshi@gmail.com>
2020-11-07 01:07:30 +0900

    create and complete class EnumerateMatters.

--- src/com/ssadamune/preparse/EnumerateMatters.java
+++ src/com/ssadamune/preparse/EnumerateMatters.java
@@ -1,7 +1,12 @@
 package com.ssadamune.preparse;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -17,21 +22,22 @@ public class EnumerateMatters {
 
     static HashMap<String, String> Limits = new HashMap<String, String>();
     static HashMap<String, String> Notices = new HashMap<String, String>();
+    static HashMap<String, String> Facility = new HashMap<String, String>();
+    static HashMap<String, String> Parking = new HashMap<String, String>();
 
-    // その他制限事項
+    // 「その他制限事項」を解析
     // "高度地区、準防火地域、風致地区、景観地区、日影制限有"
-    static String[] limits(String tdText) {
-        if (tdText.equals("-")||tdText.equals("無")) return null;
-        return tdText.split("、|／|■|●|◆|※|・");
+    static String[] limits(String text) {
+        if (text.equals("-")||text.equals("無")) return null;
+        return text.split("、|／|■|●|◆|※|・");
     }
 
-    // その他概要・特記事項
+    // 「その他概要・特記事項」を分類
     // "担当者：XXX、設備：公営水道、本下水、都市ガス、駐車場：車庫"
-    //          => {"設備"：公営水道、本下水、都市ガス、駐車場：車庫}
-    static String[] notices(String tdText) {
-
-        if (tdText.equals("-")||tdText.equals("無")) return null;
-        Matcher m = Pattern.compile("(?:^|、)([^、：]*)：").matcher(tdText);
+    //          => ["担当者", "設備", "駐車場"]
+    static String[] sortNotices(String text) {
+        if (text.equals("-")||text.equals("無")) return null;
+        Matcher m = Pattern.compile("(?:^|、)([^、：]*)：").matcher(text);
         ArrayList<String> noticeArr = new ArrayList<String>();
         while (m.find()) {
             noticeArr.add(m.group(1));
@@ -40,8 +46,21 @@ public class EnumerateMatters {
         return noticeArr.toArray(notices);
     }
 
+    // 「その他概要・特記事項」を解析
+    // "担当者：XXX、設備：公営水道、本下水、都市ガス、駐車場：車庫"
+    //          => [["公営水道", "本下水", "都市ガス"], ["車庫"]]
+    static String[][] notices(String text) {
+        if (text.equals("-")||text.equals("無")) return new String[][] {{},{}};
+        String[][] notices = new String[2][];
+        Matcher m1 = Pattern.compile(".*設備：([^：]+)($|(、建築.*)|(、駐車場.*))").matcher(text);
+        if (m1.find()) notices[0] = m1.group(1).split("、");
+        Matcher m2 = Pattern.compile(".*駐車場：([^：]+)($|(、建築.*))").matcher(text);
+        if (m2.find()) notices[1] = m2.group(1).split("、");
+        return notices;
+    }
+
     // parse houses or mansions
-    static String parseProperty(String todofuken, int ucCode, String property) throws IOException {
+    static void parseProperty(String todofuken, int ucCode, String property) throws IOException {
         String url = property.equals("house")
                 ? "https://suumo.jp/chukoikkodate/tokyo/sc_"
                 : "https://suumo.jp/ms/chuko/tokyo/sc_";
@@ -49,76 +68,89 @@ public class EnumerateMatters {
 
         Document doc = Jsoup.connect(url).get();
         Elements thtdElements = doc.select("table[summary=表]").first().select("tr > *");
-        StringBuffer json = new StringBuffer("{\n" + "    \"ID\" : " + ucCode + ",\n");
         String curItem = "";
+        String propertyForMap = "[\"" + property + "\", \"" + todofuken + "\", \"" + ucCode + "\"]";
         for (Element thtd : thtdElements) {
             if (thtd.is("th")) {
                 curItem = thtd.children().first().text();
             } else if (thtd.is("td")) {
                 switch (curItem) {
                 case "その他制限事項" :
-                    add2Map(Limits, limits(thtd.text()), property + " " + todofuken + " " + ucCode);
+                    add2Map(Limits, limits(thtd.text()), propertyForMap);
                     break;
                 case "その他概要・特記事項" :
-                    add2Map(Notices, notices(thtd.text()), property + " " + todofuken + " " + ucCode);
+                    // add2Map(Notices, sortNotices(thtd.text()), propertyForMap);
+                    String[][] notices = notices(thtd.text());
+                    add2Map(Facility, notices[0], propertyForMap);
+                    add2Map(Parking, notices[1], propertyForMap);
                     break;
                 }
             }
         }
-        json.append("}");
-        return json.toString();
-    }
-
-    static String array2Json(String[] array) {
-        if (array == null || array.length == 0) return "\"\"";
-        StringBuffer jsonArray = new StringBuffer("[");
-        for (String str : array) {
-            jsonArray.append("\"" + str + "\", ");
-        }
-        jsonArray.append("]");
-        return jsonArray.toString();
     }
 
     static void add2Map(HashMap<String, String> map, String[] items, String property) {
         if (items == null || items.length == 0) return;
         for (String item : items) {
-            map.putIfAbsent(item.trim(), property);
+            if (item.isBlank()==false) map.putIfAbsent(item.trim(), property);
         }
     }
 
-    static void printMap (HashMap<String, String> map) {
-        System.out.println("{");
-        map.forEach((item, uc) -> {
-            System.out.println("    " + item + " : " + uc);
+    static String printMap (HashMap<String, String> map) {
+        StringBuffer str = new StringBuffer("{\n");
+        map.forEach((m, p) -> {
+            str.append("    \"" + m + "\" : " + p + "\n");
         });
-        System.out.println("}");
+        str.append("}\n");
+        return str.toString();
     }
 
     // parse the todofuken ichiran page, save the properties into maps
     static void parseTodofuken (String tdfk, int maxHousePages, int maxMansionPages) throws IOException {
         var houseCodes = SuumoParser.getHousesUcList(tdfk, maxHousePages); //20
         var mansionCodes = SuumoParser.getMansionsUcList(tdfk, maxMansionPages); //50
-        System.out.println(houseCodes.size() + " houses and " + mansionCodes.size() + " mansions found in " + tdfk);
+        int houseNum = houseCodes.size();
+        int mansionNum = mansionCodes.size();
+        System.out.println(houseNum + " houses and " + mansionNum + " mansions found in " + tdfk);
 
         int properties = 0;
         for (int nc : houseCodes) {
             parseProperty(tdfk, nc, "house");
             properties += 1;
-            if (properties % 100 == 0) System.out.println(properties +"/"+ houseCodes.size() + " houses parsed");
+            if (properties % 100 == 0) System.out.println(properties +"/"+ houseNum + " houses parsed");
         }
+        System.out.println(houseNum + " houses completed in " + tdfk);
 
         properties = 0;
         for (int nc : mansionCodes) {
             parseProperty(tdfk, nc, "mansion");
             properties += 1;
-            if (properties % 100 == 0) System.out.println(properties +"/"+ mansionCodes.size() + " mansions parsed");
+            if (properties % 100 == 0) System.out.println(properties +"/"+ mansionNum + " mansions parsed");
         }
+        System.out.println(mansionNum + " mansions completed in " + tdfk);
+        System.out.println("=====================");
+    }
+
+    static void writeLog() throws IOException{
+        Date dNow = new Date( );
+        SimpleDateFormat ft = new SimpleDateFormat ("yyyyMMdd_hhmmss");
+        File logFile = new File("C:\\Users\\zwieb\\Documents\\MDproject\\MDproject\\log\\"
+                + "EnumerateMatters_" + ft.format(dNow) + ".txt");
+        logFile.createNewFile();
+        BufferedWriter bw = new BufferedWriter(new FileWriter(logFile.getAbsoluteFile()));
+        bw.write(printMap(Limits));
+        bw.write(printMap(Facility));
+        bw.write(printMap(Parking));
+        bw.close();
+        System.out.println("文件创建成功！");
     }
 
     public static void main(String[] args) throws IOException {
-        parseTodofuken("setagaya", 2, 2);
-        printMap(Limits);
-        printMap(Notices);
+        parseTodofuken("hachioji", 17, 15);
+        parseTodofuken("edogawa", 10, 20);
+        parseTodofuken("setagaya", 20, 50);
+        System.out.println(printMap(Facility));
+        writeLog();
     }
 
 }
