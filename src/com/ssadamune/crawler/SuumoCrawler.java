0cf43529c3e4804cfc96e4a687bd423b5e73b19f
Sadamune <zwiebelshi@gmail.com>
2020-11-23 19:30:35 +0900

    Create Todofuken.java

--- src/com/ssadamune/crawler/SuumoCrawler.java
+++ src/com/ssadamune/crawler/SuumoCrawler.java
@@ -1,15 +1,11 @@
 package com.ssadamune.crawler;
 
 import java.io.IOException;
-import java.util.HashSet;
+import java.util.Set;
 import java.util.logging.Logger;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 public class SuumoCrawler {
 
@@ -18,6 +14,7 @@ public class SuumoCrawler {
     // ====================================================
 
     private HandleProperty handler;
+    static Logger log = Logger.getLogger("crawler");
 
     // ====================================================
     // constructor
@@ -35,22 +32,35 @@ public class SuumoCrawler {
     // public method
     // ====================================================
 
-    public void crawlHouse(int endPage, String... tdfkList) throws IOException {
-        for (String todofuken : tdfkList) {
-            crawlProperty(todofuken, endPage, true);
+    public void crawlHouse(int maxHouses, String... tdfkList) throws IOException {
+        log.info("<Start> Crawler work");
+        for (String tdfkName : tdfkList) {
+            crawlProperty(new Todofuken(tdfkName, maxHouses, 0), true);
         }
     }
 
-    public void crawlMansion(int endPage, String... tdfkList) throws IOException {
-        for (String todofuken : tdfkList) {
-            crawlProperty(todofuken, endPage, false);
+    public void crawlMansion(int maxMansions, String... tdfkList) throws IOException {
+        log.info("<Start> Crawler work");
+        for (String tdfkName : tdfkList) {
+            crawlProperty(new Todofuken(tdfkName, 0, maxMansions), false);
         }
     }
 
     public void crawlAll(String... tdfkList) throws IOException {
-        for (String todofuken : tdfkList) {
-            crawlProperty(todofuken, 999, true);
-            crawlProperty(todofuken, 999, false);
+        log.info("<Start> Crawler work");
+        for (String tdfkName : tdfkList) {
+            Todofuken tdfk = new Todofuken(tdfkName);
+            crawlProperty(tdfk, true);
+            crawlProperty(tdfk, false);
+        }
+    }
+
+    public void crawlAll(int maxHouses, int maxMansions, String... tdfkList) throws IOException {
+        log.info("<Start> Crawler work");
+        for (String tdfkName : tdfkList) {
+            Todofuken tdfk = new Todofuken(tdfkName, maxHouses, maxMansions);
+            crawlProperty(tdfk, true);
+            crawlProperty(tdfk, false);
         }
     }
 
@@ -58,88 +68,39 @@ public class SuumoCrawler {
     // private method
     // ====================================================
 
-    private void crawlProperty(String todofuken, int endPage, boolean isHouse) throws IOException {
+    private void crawlProperty(Todofuken tdfk, boolean isHouse) throws IOException {
         String propertyType = isHouse ? "house" : "mansion";
-        String typeFragment = isHouse ? "chukoikkodate" : "ms/chuko";
         Parser parser = isHouse ? new HouseParser() : new MansionParser();
+        Set<Integer> ncSet = isHouse ? tdfk.getAllHouses() : tdfk.getAllMansions();
 
-        HashSet<Integer> ncSet = readIchiran(todofuken, typeFragment, endPage);
         int num = 0;
-        int max = ncSet.size();
-        System.out.println("[Start] Crawler find " + max + " " + propertyType + "s in " + todofuken);
+        int maxNum = ncSet.size();
+
+        log.info("Crawler find " + maxNum + " " + propertyType + "s in " + tdfk.getName());
         for (int nc : ncSet) {
-            Document doc = readBukkengaiyo(todofuken, typeFragment, nc);
+            Document doc = readBukkengaiyo(isHouse ? tdfk.houseUrl(nc) : tdfk.mansionUrl(nc));
             Property property = parser.parse(doc);
             if (property == null)
                 continue;
             handler.handle(property);
             num += 1;
             if (num % 100 == 0)
-                System.out.println(num + "/" + max + " " + propertyType + "s handled");
+                System.out.println(num + "/" + maxNum + " " + propertyType + "s handled");
         }
         parser.outputSurpirses();
-        System.out.println("[Finish] " + num + " " + propertyType + "s in " + todofuken + " crawled");
-    }
-
-    /**
-     * read ichiran-page at suumo
-     * 
-     * @param tdfk    todofuken name
-     * @param typeFragment    "ms/chuko" for mansion, "chukoikkodate" for house
-     * @param endPage how many pages to read
-     * @return a set of nc codes
-     * @throws IOException
-     */
-    private static HashSet<Integer> readIchiran(String tdfk, String typeFragment, int endPage) throws IOException {
-
-        /** all the uc-code of this todofuken, use HashSet to avoid duplicate values */
-        HashSet<Integer> ncSet = new HashSet<>();
-
-        if (endPage <= 0)
-            return ncSet;
-
-        /** regex pattern of link, which included the uc-code */
-        String pattern = typeFragment + "/tokyo/sc_" + tdfk + "/nc_(\\d*)(/)";
-
-        int curPage = 1;
-        while (curPage <= endPage) {
-            String url = "https://suumo.jp/" + typeFragment + "/tokyo/sc_" + tdfk + "/pnz1" + curPage + ".html";
-            Document doc = Jsoup.connect(url).get();
-
-            /** set endPage no more than the maxPage */
-            if (curPage == 1)
-                endPage = Math.min(endPage, maxPage(doc));
-
-            /** extract nc-code from all links in the document */
-            for (Element link : doc.select("a[href]")) {
-                String curLink = link.attr("href");
-                Matcher m = Pattern.compile(pattern).matcher(curLink);
-                if (m.find()) {
-                    ncSet.add(Integer.parseInt(m.group(1)));
-                }
-            }
-            curPage++;
-        }
-        return ncSet;
-    }
-
-    private static int maxPage(Document doc) throws IOException {
-        Elements pages = doc.select("ol[class=pagination-parts]").first().select("li");
-        return Integer.parseInt(pages.last().text());
+        log.info("<Finish> " + num + " " + propertyType + "s in " + tdfk.getName() + " crawled");
     }
 
     /**
      * read bukkengaiyo-page
      * 
-     * @param tdfk   todofuken name
-     * @param typeFragment   "ms/chuko" for mansion, "chukoikkodate" for house
-     * @param ncCode code of property
+     * @param tdfk         todofuken name
+     * @param typeFragment "ms/chuko" for mansion, "chukoikkodate" for house
+     * @param ncCode       code of property
      * @return a json Document of current page
      * @throws IOException
      */
-    private static Document readBukkengaiyo(String tdfk, String typeFragment, int ncCode) throws IOException {
-        String url = "https://suumo.jp/" + typeFragment + "/tokyo/sc_" + tdfk + "/nc_" + ncCode + "/bukkengaiyo/";
-        Logger log = Logger.getLogger("crawler");
+    private static Document readBukkengaiyo(String url) throws IOException {
         Document doc = null;
         try {
             doc = Jsoup.connect(url).get();
@@ -158,8 +119,9 @@ public class SuumoCrawler {
         // FeaturesCollector()});
 
         SuumoCrawler sc = new SuumoCrawler();
-        sc.crawlAll("ome");
-        sc.crawlHouse(3, "setagaya");
-
+        // sc.crawlAll("ome");
+        // sc.crawlAll(200, 200, "setagaya", "shinjuku");
+        sc.crawlHouse(100, "nerima");
+        sc.crawlMansion(100, "nerima");
     }
 }
---
+++ src/com/ssadamune/crawler/Todofuken.java
@@ -0,0 +1,123 @@
+package com.ssadamune.crawler;
+
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+class Todofuken {
+
+    // ====================================================
+    // field
+    // ====================================================
+
+    private String name;
+    private Integer maxHouses;
+    private Integer maxMansions;
+    private HashSet<Integer> allHouses;
+    private HashSet<Integer> allMansions;
+
+    // ====================================================
+    // constructor
+    // ====================================================
+
+    Todofuken(String name) {
+        this.name = name;
+    }
+
+    Todofuken(String name, int maxHouses, int maxMansions) {
+        this.name = name;
+        this.maxHouses = maxHouses;
+        this.maxMansions = maxMansions;
+    }
+
+    // ====================================================
+    // public
+    // ====================================================
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<Integer> getAllHouses() {
+        if (allHouses == null)
+            this.allHouses = bulidSet(maxHouses, "chukoikkodate");
+        return allHouses;
+
+    }
+
+    public Set<Integer> getAllMansions() {
+        if (allMansions == null)
+            this.allMansions = bulidSet(maxMansions, "ms/chuko");
+        return allMansions;
+    }
+
+    String houseUrl(int nc) {
+        return "https://suumo.jp/chukoikkodate/tokyo/sc_" + name + "/nc_" + nc + "/bukkengaiyo/";
+    }
+
+    String mansionUrl(int nc) {
+        return "https://suumo.jp/ms/chuko/tokyo/sc_" + name + "/nc_" + nc + "/bukkengaiyo/";
+    }
+
+    // ====================================================
+    // private method
+    // ====================================================
+
+    private HashSet<Integer> bulidSet(Integer maxProperties, String urlFragment) {
+        HashSet<Integer> set = new HashSet<>();
+
+        /** regex pattern of link, which included the uc-code */
+        String pattern = urlFragment + "/tokyo/sc_" + this.name + "/nc_(\\d*)(/)";
+
+        int curPage = 1;
+        int endPage = 0;
+        int num = 0;
+
+        do {
+            String url = "https://suumo.jp/" + urlFragment + "/tokyo/sc_" + this.name + "/pnz1" + curPage + ".html";
+            Document doc = readIchiran(url);
+
+            /** set endPage */
+            if (curPage == 1) {
+                Elements pages = doc.select("ol[class=pagination-parts]").first().select("li");
+                endPage = Integer.parseInt(pages.last().text());
+            }
+
+            /** extract nc-code from all links in the document */
+            for (Element link : doc.select("a[href]")) {
+                String curLink = link.attr("href");
+                Matcher m = Pattern.compile(pattern).matcher(curLink);
+                if (m.find() && set.add(Integer.parseInt(m.group(1)))) {
+                    num++;
+                    if (maxProperties != null && maxProperties <= num) {
+                        System.out.println("======== max " + maxProperties + ", current " + num);
+                        break;
+                    }
+                }
+            }
+            curPage++;
+
+        } while (curPage <= endPage && (maxProperties == null || maxProperties > num));
+
+        return set;
+    }
+
+    private Document readIchiran(String url) {
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(url).get();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return doc;
+    }
+
+}
